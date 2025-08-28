c4bf911ab65442a5c8dd01ac5bd3f486500563c3
tengkuzainul <tengkuzainula04@gmail.com>
2025-08-28 12:38:46 +0700

    change: grid system in project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -102,7 +102,7 @@ export default function AllProjectsPage() {
 
         {/* Projects Grid */}
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 items-stretch"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 items-stretch"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.6, delay: 0.3 }}
